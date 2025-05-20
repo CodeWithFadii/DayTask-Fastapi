fca73bdfbd0cba29cae33600289345de7252d548
Fahad Ali <115652088+CodeWithFadii@users.noreply.github.com>
2025-05-20 13:53:34 +0500

    Remove additional router and related image processing functions

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,6 @@
 from passlib.context import CryptContext
 
 from passlib.context import CryptContext
-import cv2
-import numpy as np
-from PIL import Image
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -15,11 +12,3 @@ def get_password_hash(password: str):
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
-
-
-def preprocess_image(image_bytes):
-    image_np = np.frombuffer(image_bytes, np.uint8)
-    image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
-    return Image.fromarray(thresh)
